e7486ef2772ffd325132f583c0c792466cd9c0ca
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-23 21:56:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,9 @@ def long_planeteer_calls(calls)
   end 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.any? do |word|
+    if word == cheddar or gouda or camembert 
 end
