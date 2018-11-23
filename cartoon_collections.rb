3318d4c86d2f67fffe2844878dfdedcf6cb004a7
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-23 21:57:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,7 @@ end
 def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  list.any? do |word|
-    if word == cheddar or gouda or camembert 
+  list.find do |word|
+    word.cheese_types?
+  end 
 end
