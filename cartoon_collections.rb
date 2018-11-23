27ae5d81524b846e31026a7da120c04a8caa7d1b
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-23 21:38:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  planeteer_calls.map do |calls|
+    
 end
 
 def long_planeteer_calls# code an argument here
